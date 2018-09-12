d796be950f533aa698f684ec9389ae4c47cf5f71
Jared Gesser <6437182+peanutbother@users.noreply.github.com>
2018-09-12 15:00:01 +0200

    demonstration of a semi-fix
    you definitely should use a better solution like jQuery's ready function (`$(function(){})`)

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import * as d3 from "d3";
 
-console.log("test");
+setTimeout(() => {
+  console.log("test");
+  let svg = d3.select("#map");
+  let circle = svg
+    .selectAll("circle")
+    .data([10, 20, 30])
+    .enter()
+    .append("circle")
+    .style("fill", "steelblue")
+    .attr("cx", d => d * 10)
+    .attr("cy", 30)
+    .attr("r", d => d);
 
-let svg = d3.select("#map");
-let circle = svg.selectAll("circle").data([10,20,30])
-.enter().append("circle")
-.style("fill", "steelblue")
-.attr("cx", d=>d*10)
-.attr("cy", 30)
-.attr("r", d=>d)
-;
-
-
-console.log("after");
+  console.log("after");
+}, 500);
